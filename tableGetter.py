e7142527b1352d3c165130be4af8d530e381008f
tmcnairbledsoe <tmcnairbledsoe@gmail.com>
2023-10-28 19:23:44 -0400

    cleanup

--- tableGetter.py
+++ tableGetter.py
@@ -22,6 +22,25 @@ def SpyHistory():
     
     return df
 
+def QqqHistory():
+    df = pd.read_csv('C:\\src\\stockPredictor\\QqqHist.csv', index_col=0)
+    df['DateTime'] = pd.to_datetime(df['DateTime'])
+    df['date'] = [d.date() for d in df['DateTime']]
+    df['time'] = [d.time() for d in df['DateTime']]
+    df['Open'] = df['Open'].astype(float)
+    df['Close'] = df['Close'].astype(float)
+    df['Volume'] = df['Volume'].astype(int)
+    df['slowsma'] = df['Close'].rolling(21).mean()
+    df['fastsma'] = df['Close'].rolling(9).mean()
+    df['Difference'] = df['Close'] - df['Open']
+    df = df.dropna()
+
+    for currentDate in df.date.unique():
+        if len(df.date.unique()) > len(df.loc[df['date'] == currentDate]):
+            df = df.loc[df['date'] != currentDate]
+    
+    return df
+
 def TradeData():
     df = pd.read_csv('C:\\src\\stockPredictor\\tradeData.csv', index_col=0)
     df['Price'] = df['Price'].astype(float)
